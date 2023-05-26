e3713b8c9dd1f2af222f763ce86771b35ad06de5
mohammedjai <ajola5138@gmail.com>
2023-05-26 10:10:38 +0000

    new appdate

--- amazon.py
+++ amazon.py
@@ -1,79 +1,67 @@
 from bs4 import BeautifulSoup
-from time import sleep
 import requests
 import pandas as pd
 
 
 
 
-website = "https://www.amazon.com/s?k=shoes&i=fashion-mens-intl-ship&crid=2AQQ0F5VA08X&sprefix=sh%2Cfashion-mens-intl-ship%2C442&ref=nb_sb_ss_ts-doa-p_1_2"
+website = "https://www.amazon.com/s?k=gaming+laptop&crid=5VA0UYIWTL90&sprefix=gaming+laptop%2Caps%2C200&ref=nb_sb_noss_1"
 
 header = ({"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36","Accept-Language":"ar-MA,ar;q=0.9,en-US;q=0.8,en;q=0.7","Accept-Encoding":"gzip, deflate, br"})
-#send request
+
 r = requests.get(website, headers=header)
+
 
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-pack = soup.find_all("div", {"class":"a-section a-spacing-base"})
+result = soup.find_all("div",{"class":"a-section a-spacing-small a-spacing-top-small"})
 
 
-name = []
-price = []
-real_price = []
-delivery_price = []
-rating = []
-rating_count = []
 
 
-for info in pack :
+for results in result :
 
-    name = []
+    configuration = []
     try :
-        name.append(info.find("span", {"class":"a-size-base-plus a-color-base a-text-normal"}).text)
+        configuration.append(results.find("span", {"class":"a-size-medium a-color-base a-text-normal"}).text)
     except :
-        print("n/a")
+        continue
+    
+
 
     price = []
     try :
-        price.append(info.find("span", {"class":"a-offscreen"}).text)
+        price.append(results.find("span", {"class":"a-offscreen"}).text) 
     except :
-        print("n/a")
+        continue
+    
 
     real_price = []
     try :
-        real_price.append(info.find("span", {"class":"a-price a-text-price"}).text)
+        real_price.append(results.find("span", {"class":"a-price a-text-price"}).text)
     except :
-        print("n/a")
+        continue
     
+
+
     delivery_price = []
     try :
-        delivery_price.append(info.find("div", {"class":"a-row a-size-base a-color-secondary s-align-children-center"}).text)  
+        delivery_price.append(results.find("div", {"class":"a-row a-size-base a-color-secondary s-align-children-center"}).text)
     except :
-        print("n/a")
+        continue
+    
 
     rating = []
     try :
-        rating.append(info.find("span", {"class":"a-icon-alt"}).text)
+        rating.append(results.find("div",{"class":"a-row a-size-small"}).text)   
     except :
-        print("n/a")
+        continue
     
-    rating_count = []
-    try :
-        rating_count.append(info.find("span", {"class":"a-size-base s-underline-text"}).text)
-    except :
+    laptops = []
+    laptop = ({"Configuration":configuration , "Price":price , "Real Price":real_price , "Delivery Price":delivery_price , "Rating":rating})
+    laptops.append(laptop)
+
+    df = pd.DataFrame(laptops)
+    print(df.to_string(index=False).encode('utf-8'))
     
-        print("n/a")
-    
-
-
-
-
-
-
-
-
-
-
-
-
